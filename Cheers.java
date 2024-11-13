697a55c878e921280e8db94d4b15895311782a63
Alon Hagity <alonhagity@Alons-MacBook-Air.local>
2024-11-13 21:27:35 +0200

    adding Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,36 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	String word = args[0];
+        word = word.toUpperCase();
+        int num = Integer.parseInt(args[1]);
+        char[] test = {'A','E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'};
+        char e;
+        boolean isSame = false;
+
+        for(int i = 0; i < word.length(); i++) {
+            e = word.charAt(i); 
+            isSame = false;
+            System.out.print("Give me a");
+
+            //testing for letters to print "an" insstead of "a"
+            for(int j = 0; j < test.length; j++){
+                if(e == test[j]) {
+                        System.out.print("n ");
+                        isSame = true;
+                        break;
+                }
+                
+            }  
+
+            if(isSame != true) System.out.print("  ");
+            System.out.println(word.charAt(i) + "!");
+        }
+
+        System.out.println("What does that spell??");
+        for(int i = 0; i < num; i++) {
+                System.out.println(word + "!!!");
+        }
+
         }
 }
