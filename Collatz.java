204bd5ee896b712dbe5231f7212f320930d97e5a
Alon Hagity <alonhagity@Alons-MacBook-Air.local>
2024-11-14 18:57:21 +0200

    Collatz version 0

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,38 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int highestSeed = Integer.parseInt(args[0]);
+		if(args[1].equals("v")) {
+
+			int seed = 1;
+			int startPoint = 1;
+			int countItems = 1;
+			boolean oneInBeggining = true;
+
+			while(startPoint < highestSeed + 1) {
+				countItems = 1;
+				oneInBeggining = true;
+				seed = startPoint;
+
+				while((seed != 1) || (oneInBeggining == true)) {
+					oneInBeggining = false;
+					System.out.print(seed + " ");
+					if(seed % 2 == 0) {
+						seed /= 2;
+					} else {
+						seed = seed * 3 + 1;
+					}
+					countItems++;
+				}
+				System.out.print(1 + " ");
+				System.out.println("(" + countItems + ")");
+
+				startPoint++;
+			}
+			System.out.println("Every one of the first " + highestSeed + " hailstone sequences reached 1.");
+		} else {
+			System.out.println("Every one of the first " + highestSeed + " hailstone sequences reached 1.");
+		}
+
 	}
 }
